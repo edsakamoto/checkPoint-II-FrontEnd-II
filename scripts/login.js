72054becff91197658d30053a12817beee38044a
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-04-16 21:24:14 -0300

    add validações no login e signup

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,31 @@
 let inputEmailBtnReference = document.querySelector("#inputEmail");
 let inputPasswordBtnReference = document.querySelector("#inputPassword");
 let submitBtnReference = document.querySelector('#submitBtn');
+let controladoresReference = document.querySelectorAll('.controlador');
+/*     users criados para teste
+user: teste@teste.com / senha: 12345
+user: killbill@django.com   / senha: django
+*/
+
+for(let controladorReferencia of controladoresReference){
+    
+    let inputReferencia = controladorReferencia.children[0]
+
+    inputReferencia.addEventListener('keyup',event =>{
+
+       if(inputReferencia.checkValidity()){
+    
+            controladorReferencia.classList.remove('erro')
+            submitBtnReference.disabled = false            
+    
+        } else {
+    
+            controladorReferencia.classList.add('erro')
+            submitBtnReference.disabled = true
+    
+        }    
+    })   
+}
 
 submitBtnReference.addEventListener('click',event =>{
     
@@ -28,14 +53,25 @@ submitBtnReference.addEventListener('click',event =>{
 
     fetch('https://ctd-todo-api.herokuapp.com/v1/users/login',requestConfiguration)
         .then(response =>{
-                response.json() 
-                    .then(data => {
-                            // console.log(data.jwt);
-                            localStorage.setItem('token',data.jwt)
-                            window.location.href = './tarefas.html'
+            console.log(response)
+            if(response.status === 404){
+
+                alert('Usuário não encontrado. Tente novamente.')
+                inputEmailBtnReference.value = ''
+                inputPasswordBtnReference.value = ''
+
+            } else if (response.status === 400){
+
+                alert('Senha incorreta. Digite novamente.')
+                inputPasswordBtnReference.value = ''
+
+            } else if (response.ok){
+
+                response.json()
+                    .then(data => {                        
+                        localStorage.setItem('token',data.jwt)
+                        window.location.href = './tarefas.html'
                     })
+            } else {alert('Erro no servidor')}            
         })   
-
-
 })
-
--- scripts/signup.js
+++ scripts/signup.js
@@ -3,10 +3,51 @@ let firstNameReference = document.querySelector('#firstName');
 let lastNameReference = document.querySelector('#lastName');
 let emailCadastroReference = document.querySelector('#emailCadastro');
 let passwordCadastroReference = document.querySelector('#passwordCadastro');
+let controladoresReference = document.querySelectorAll('.controlador');
+let validador = []
+
+for(let controladorReferencia of controladoresReference){
+    
+    let inputReferencia = controladorReferencia.children[0]
+    let smallReferencia = controladorReferencia.children[1]
+    inputReferencia.addEventListener('keyup',event =>{
+
+       if(inputReferencia.checkValidity()){
+    
+            controladorReferencia.classList.remove('erro')
+            createUserReference.disabled = false 
+            /* if para validar se as senhas são iguais */
+            if(inputReferencia.type === 'password' && inputReferencia.id === 'passwordCadastro'){                
+                
+                validador[0] = (inputReferencia.value)
+                
+            } else if (inputReferencia.type === 'password' && inputReferencia.id === 'repetirPassword'){
+                
+                validador[1] = (inputReferencia.value)
+                
+                if(validador[0] !== validador[1]){
+
+                    smallReferencia.innerHTML = 'A senha repetida está diferente. Digite novamente '
+                    controladorReferencia.classList.add('erro')
+                    createUserReference.disabled = true
+
+                }
+            }
+    
+        } else {
+    
+            controladorReferencia.classList.add('erro')
+            createUserReference.disabled = true
+    
+        }    
+    })   
+}
 
 createUserReference.addEventListener('click',event =>{
     
     event.preventDefault();
+
+    validador.splice(0, validador.length) //somente para esvaziar os dados gravados, validador foi utilizado durante o listener do keyup
 
     let users = {
 
@@ -34,13 +75,11 @@ createUserReference.addEventListener('click',event =>{
         .then(response =>{
                 response.json() 
                     .then(data => {
-                            // console.log(data.jwt);
-                            localStorage.setItem('token',data.jwt);
-                            // if(data.ok){
-                            //     window.location.href = './index.html'                                
-                            // }
+
+                            localStorage.setItem('token',data.jwt);                            
+                            window.location.href = './index.html';
+
                     })
         })
-
 
 })
