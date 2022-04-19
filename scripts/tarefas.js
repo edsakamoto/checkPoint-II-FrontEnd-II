7e9bd1616a6c5f9ed4ccefd59d1e7b4666c523ea
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-04-18 21:43:49 -0300

    limpando o codigo

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -5,7 +5,6 @@ let tarefasPendentesReference = document.querySelector('.tarefas-pendentes');
 let tarefasTerminadasReference = document.querySelector('.tarefas-terminadas');
 let closeAppReference = document.querySelector('#closeApp');
 
-//let dados = [];
 let requestHeaders = {
     'Content-Type': 'application/json',
     'Authorization': localStorage.getItem('token')
@@ -15,8 +14,7 @@ let requestConfiguration = {
 };
 
 let requestConfigurationPut = {
-    method:'PUT',
-    // body: JSON.stringify({completed: true}), //adicionado essa body dentro da função como parametro deixando mais dinamico
+    method:'PUT',    
     headers: requestHeaders
 }
 
@@ -30,13 +28,9 @@ fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe',requestConfiguration)
     .then(response =>{
             response.json()
                 .then(data =>{
-                   // console.log(data)
+                   
                     userNameReference.innerHTML += `${data.firstName} ${data.lastName}`;
-                    //dados.push(data);
-                    localStorage.setItem('idUser',data.id)
-                    localStorage.setItem('firstNameUser',data.firstName)
-                    localStorage.setItem('lastNameUser',data.lastName)
-                    localStorage.setItem('emailUser',data.email)
+                   
                 })
     })
 
@@ -52,10 +46,7 @@ cadastroTarefaReference.addEventListener('click',event =>{
             method:'POST',
             body: JSON.stringify(posts),
             headers: requestHeaders
-        }
-
-        // let skeletonReference = document.querySelector('#skeleton');
-        // skeletonReference.style.display = 'none'
+        }        
 
         novaTarefaReference.value = ''
 
@@ -64,26 +55,7 @@ cadastroTarefaReference.addEventListener('click',event =>{
         fetch('https://ctd-todo-api.herokuapp.com/v1/tasks',requestConfigurationPost)
             .then(response =>{
                 if(response.ok){
-
-                    // response.json()
-                    //     .then(data =>{
-                    //         console.log(data)
-                    //         const dataAtal = new Date(data.createdAt);
-                    //         const dataFormatada = dataAtal.toLocaleDateString('pt-BR',{
-                    //             day:'2-digit',
-                    //             month:'2-digit',
-                    //             year:'numeric'
-                    //         })
-                    //         tarefasPendentesReference.innerHTML += `
-                    //         <div id="">
-                    //         <li class="tarefa">
-                    //         <div class="not-done" onclick="atualizaTarefa(${data.id},'false')"></div>
-                    //         <div class="descricao">
-                    //         <p class="nome">${data.description}</p>
-                    //         <p class="timestamp">Criada em: ${dataFormatada}</p>
-                    //         </div>
-                    //         `
-                    //     })
+                   
                     obterLista();
 
                 } else {
@@ -103,9 +75,7 @@ function obterLista(){
            
             response.json()
                 .then(tasks =>{
-                    console.log(tasks)
-                    console.log(tasks.length)
-
+                    
                     if(tasks.length === 0){
 
                         /* somente para manter a estrutura skeleton caso n encontre nenhum valor no objeto retornado */
@@ -123,14 +93,10 @@ function obterLista(){
                         /*--reseta a "lista" do html atual para n duplicar ao passar nas funcoes atualizaTarefa() e deletarTarefa()--*/
                     tarefasTerminadasReference.innerHTML = ''
                     tarefasPendentesReference.innerHTML = ''
-                    /*-------------------------------------*/
-                    //let skeletonReference = document.querySelector('#skeleton');
-                    // let tarefasPendentesReference = document.querySelector('.tarefas-pendentes');
-                   
-                    //skeletonReference.style.display = 'none'
+                    /*-------------------------------------*/                   
 
                     for(task in tasks){
-                        // console.log(tasks[task].createdAt.substring(0,10).toLocaleDateString())
+                   
                         const dataAtual = new Date(tasks[task].createdAt);
                         const dataFormatada = dataAtual.toLocaleDateString('pt-BR',{
                             day:'2-digit',
